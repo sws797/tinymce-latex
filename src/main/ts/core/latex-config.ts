e68770db5b0f940a83dff4239c92566528d10913
张喜硕 <zhangxishuo1998@outlook.com>
2020-04-16 13:53:00 +0800

    remove-mathjax-conf

--- src/main/ts/core/latex-config.ts
+++ src/main/ts/core/latex-config.ts
@@ -1,5 +1,3 @@
-import { LatexSpec } from '../spec/latex.spec';
-
 /**
  * 公式配置
  */
@@ -8,19 +6,11 @@ export class LatexConfig {
   /** 公式渲染栏 id */
   public renderIframeID: string;
 
-  /** 用户自定义配置 */
-  public conf: LatexSpec;
-
   /** 外部 JavaScript 文件地址列表 */
   public scripts: Array<string>;
 
   constructor(editor: any) {
     /** 获取公式渲染栏 id */
     this.renderIframeID = editor.dom.uniqueId();
-    /** 获取用户自定义配置 */
-    this.conf = editor.settings.latex;
-    /** 配置第三方 JavaScript 脚本 */
-    this.scripts = new Array<string>();
-    this.scripts.push(this.conf.mathJax.lib);
   }
 }
--- src/main/ts/spec/latex.spec.ts
+++
@@ -1,9 +0,0 @@
-import { MathJaxSpec } from './math-jax.spec';
-
-/**
- * Latex 配置规格
- */
-export class LatexSpec {
-  /** MathJax 配置子项 */
-  public mathJax: MathJaxSpec;
-}
